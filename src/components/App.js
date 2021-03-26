de90d2187546c2381a0deff964f15cf4440257f8
Mark Taylor <taylor26@seattleu.edu>
2021-03-26 12:34:49 -0700

    bare bones app

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import '../css/App.css';
+
+
+//sdaasdasda
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        hello world
+      </header>
+    </div>
+  );
+}
+
+export default App;
