67ee278c3ae436dbbfe53a1ea4355a208c24ec78
Mark Taylor <taylor26@seattleu.edu>
2021-03-27 12:44:44 -0700

    added css to display cards
    need to fix body css to make changes to body css to make things line up by row.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,42 @@
 import '../css/App.css';
 import Deck from './deck.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function App() {
+function App()
+{
     const deck = new Deck()
     deck.shuffle()
     console.log(deck.cards)
 
     return(
-        <div className="App">
-        <header>
-            hello world
-        </header>
-        </div>
+        <>
+            <div className="App">
+            </div>
+
+            <div className="container">
+                <div className ="row">
+                    <div className="col-4">
+                        <div className ="deck">
+                            52
+                        </div>
+                    </div>
+
+                    <div className="col-4">
+                        <div className="text"> dealer hand </div>
+                    </div>
+
+                    <div className="col-4">
+                        <div className="curr-card card" data-value = "9 ♠">
+                            ♠
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="text"> player hand </div>
+        </>
     );
 }
 
